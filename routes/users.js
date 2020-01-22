c02c9662d41909bf0f62bf438c0e0c500d448616
naomiskevo <naomiskevo@gmsail.com>
2020-01-22 17:36:35 -0600

    adds basic routes and finishes signup functionality

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/users');
+
+router.post('/signup', userCtrl.signup);
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,20 +2,29 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(express.json());
+
+require('dotenv').config();
+require('./config/database');
 
 // Configure both serve-favicon & static middlewares
 // to serve from the production 'build' folder
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+
+const userRoutes = require('./routes/users')
 // Put API routes here, before the "catch all" route
 
-
+app.use('/api/users', userRoutes);
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
