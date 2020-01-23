2735045acef2c3fd24748b2ae402296a77ffdc22
naomiskevo <naomiskevo@gmsail.com>
2020-01-23 04:26:28 -0600

    sets up NavBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Signup from './user/Signup'
-import Login from './user/Login'
+import Signup from './userPages/SignupPage'
+import Login from './userPages/LoginPage'
+import Home from './corePages/HomePage'
+import NavBar from './corePages/NavBar'
 
 
 
@@ -9,7 +11,9 @@ import Login from './user/Login'
 const App = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Switch>
+        <Route exact path='/'  component={Home} />
         <Route exact path='/login'  component={Login} />
         <Route exact path='/signup'  component={Signup} />
       </Switch>
---
+++ src/corePages/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link, withRouter} from 'react-router-dom'
+
+
+const NavBar = () => (
+    <div>
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/signup">Signup</Link>
+            </li>
+        </ul>
+    </div>
+)
+
+
+export default withRouter(NavBar)
