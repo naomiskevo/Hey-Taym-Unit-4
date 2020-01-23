488285dfb69b1c3d27f82a027c54507dbabe72e6
naomiskevo <naomiskevo@gmsail.com>
2020-01-23 03:52:40 -0600

    adds update and viewing one user profile functionalities

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,34 @@ const User = require('../models/user');
 
 module.exports = {
     userById,
+    getOne,
+    update
 }
+
+function getOne(req, res) {
+    req.profile.hased_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile);
+}
+
+function update(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $set: req.body },
+        { new: true },
+        (err, user) => {
+            if (err) {
+                return res.status(400).json({
+                    err: 'You are not authorized to preform this action'
+                });
+            }
+            user.hased_password = undefined
+            user.salt = undefined
+            res.json(user);
+        }
+    );
+    
+};
 
 function userById(req, res, next, id) {
     User.findById(id).exec((err, user) => {
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,20 @@ const router = express.Router();
 const userCtrl = require('../controllers/users');
 const { requireLogin, isAdmin, isAuth } = require('../controllers/auth');
 
+router.put('/:userId', requireLogin, isAuth, userCtrl.update);
+router.get('/:userId', requireLogin, isAuth, userCtrl.getOne);
+
+router.get(
+    '/secret/:userId',
+    requireLogin,
+    isAdmin,
+    isAuth,
+    (req, res) => {
+        res.json({
+            user: req.profile
+        })
+    });
+
 router.param('userId', userCtrl.userById);
-
-router.get('/secret/:userId', requireLogin, isAdmin, isAuth, (req, res) => {
-    res.json({
-        user: req.profile
-    })
-});
-
 
 module.exports = router;
