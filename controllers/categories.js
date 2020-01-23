f4cc51e71764debe9ad7a1c8d2ac8bfcbfddbbec
naomiskevo <naomiskevo@gmsail.com>
2020-01-22 22:19:16 -0600

    adds functionality to create a category

---
+++ controllers/categories.js
@@ -0,0 +1,18 @@
+const Category = require('../models/category');
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+module.exports = {
+    create,
+}
+
+function create(req, res) {
+    const category = new Category(req.body)
+    category.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                err: errorHandler(err)
+            });
+        }
+        res.json({ data });
+    });
+};
--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const catCtrl = require('../controllers/categories');
+const { requireLogin, isAdmin, isAuth } = require('../controllers/auth');
+const { userById } = require('../controllers/users');
 
 
-router.post('/create', catCtrl.create);
+router.post(
+    '/create/:userId',
+    requireLogin,
+    isAuth,
+    isAdmin,
+    catCtrl.create
+);
 
+
+
+router.param('userId', userById);
 
 
 
