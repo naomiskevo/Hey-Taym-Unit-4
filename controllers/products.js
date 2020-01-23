e158a26110afb8a3929d03f19eac6c0ef23fe929
naomiskevo <naomiskevo@gmsail.com>
2020-01-23 00:06:48 -0600

    adds validation to creating product function

--- controllers/products.js
+++ controllers/products.js
@@ -17,9 +17,22 @@ function create(req, res) {
                 err: 'Image could not be uploaded'
             })
         }
+        const { name, price, category, quantity, shipping } = fields
+
+        if (!name || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                err: 'All fields are required'
+            });
+        }
+
         let product = new Product(fields)
 
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    err: 'Image should be less than 1mb in size'
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
