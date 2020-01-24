48b4917fee23add0302277463c4f478afeef9eed
naomiskevo <naomiskevo@gmsail.com>
2020-01-23 20:20:13 -0600

    renders new arrival and best seller products on homepage

--- src/corePages/Card.jsx
+++ src/corePages/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import ShowImage from './ShowImage'
 
 const Card = ({ product }) => {
     return (
         <div className="col-4 mb-3">
             <div className="card">
-                <div className="card-headr">{product.name}</div>
+                <div className="card-header">{product.name}</div>
                 <div className="card-body">
-                    <p>{product.description}</p>
+                    <ShowImage item={product} url="products" />
+.                    <p>{product.description}</p>
                     <p>{product.price}</p>
                     <Link to="/">
                         <button className="btn btn-outline-primary mt-2 mb-2">
